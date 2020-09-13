cb0e00b6741c55adc20327e1ff01c0067b2ac495
DELL <DELL@WINCTRL-O8TBVO9>
2020-09-13 11:07:14 +0530

    Push from project

--- src/org/qsp/weekend/ioDemo/Dummy.java
+++ src/org/qsp/weekend/ioDemo/Dummy.java
@@ -1,7 +1,11 @@
 package org.qsp.weekend.ioDemo;
 
+
 public class Dummy {
   int dummyVariable;
-  //To check how to pull changes in Eclips
+  //To check how to pull changes in Eclipse
+  public static void main(String[] args) {
+	System.out.println("HI...!");
+}
 
 }
